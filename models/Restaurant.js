ce7e9f655edcf925d42c4115c8affa7d1df19563
Marianna <mddobal@gmail.com>
2019-02-11 10:12:33 -0200

    models created

---
+++ models/Restaurant.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const restSchema = new Schema(
+  {
+    name: String,
+    type: { type: String, enum: ['coffee shop', 'fast food', 'bar'] },
+    description: String,
+    address: { type: String },
+    location: { type: { type: String }, coordinates: [Number] }
+  },
+  {
+    timestamps: true
+  }
+);
+
+restSchema.index({ location: '2dsphere' });
+
+const Restaurant = mongoose.model('Restaurant', restSchema);
+
+module.exports = Restaurant;
